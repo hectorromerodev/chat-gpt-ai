b764877577e28f2a4c44c05354ec694ef95fa3fc
Hector Romero <hectorromerodev@gmail.com>
2023-02-12 12:10:22 -0800

    update whitelist for external files

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+        'links.papareact.com'
+    ]
+  },
   experimental: {
     appDir: true,
   },
+  extends: [
+    'plugin:@next/next/recommended',
+    "prettier"
+  ],
 }
 
 module.exports = nextConfig
