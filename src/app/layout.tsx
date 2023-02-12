06a9937db0089e2c2c79adbc88380f2e08a423a6
Hector Romero <hectorromerodev@gmail.com>
2023-02-12 12:13:04 -0800

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,44 @@
 import './globals.css'
+import Login from "@/app/components/Login";
+import SessionProvider from "@/app/components/SessionProvider";
+import SideBar from "@/app/components/SideBar";
+import {authOptions} from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+    const session = await getServerSession(authOptions);
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body>
+        <SessionProvider session={session}>
+            {
+                !session
+                    ? (
+                        <Login />
+                       )
+                    : (
+                        <div className="flex">
+                            <div className="bg-[#202123] m-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
+                                <SideBar />
+
+                            </div>
+
+                            {/*Client provider - notification*/}
+
+                            <div className="bg-[#343541] flex-1">
+                                {children}
+                            </div>
+                        </div>
+                    )
+            }
+
+        </SessionProvider>
+      </body>
     </html>
   )
 }
