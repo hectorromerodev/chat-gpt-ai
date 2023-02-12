1dac78915f80fdd0d397d5abc11a34a117ed5e6b
Hector Romero <hectorromerodev@gmail.com>
2023-02-12 12:11:49 -0800

    update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,68 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import {SunIcon, BoltIcon, ExclamationTriangleIcon} from "@heroicons/react/24/outline";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-col items-center justify-center h-screen px-2 text-white">
+        <h1 className="text-5xl font-bold mb-20">ChatGPT</h1>
+
+        <div className="flex space-x-2 text-center">
+
+            <section>
+                <div className="flex flex-col items-center justify-center mb-5">
+                    <SunIcon className="w-8 h-8" />
+                    <h2>Examples</h2>
+                </div>
+                <div className="space-y-2">
+                    <p className="infoText">
+                        &quot;Explain something to me&quot;
+                    </p>
+                    <p className="infoText">
+                        &quot;What is the difference between ad dog and a cat?&quot;
+                    </p>
+                    <p className="infoText">
+                        &quot;What is the color of the sun?&quot;
+                    </p>
+                </div>
+            </section>
+
+            <section>
+                <div className="flex flex-col items-center justify-center mb-5">
+                    <BoltIcon className="w-8 h-8" />
+                    <h2>Capabilities</h2>
+                </div>
+                <div className="space-y-2">
+                    <p className="infoText">
+                        &quot;Change the ChatGTP model to use.&quot;
+                    </p>
+                    <p className="infoText">
+                        &quot;Messages are stored in Firebase; Firestore.&quot;
+                    </p>
+                    <p className="infoText">
+                        &quot;Hot toast notifications when ChatGTP is thinking!&quot;
+                    </p>
+                </div>
+            </section>
+
+            <section>
+                <div className="flex flex-col items-center justify-center mb-5">
+                    <ExclamationTriangleIcon className="w-8 h-8" />
+                    <h2>Limitations</h2>
+                </div>
+                <div className="space-y-2">
+                    <p className="infoText">
+                        &quot;May occasionally produce harmful instructions or biased content.&quot;
+                    </p>
+                    <p className="infoText">
+                        &quot;May occasionally generate incorrect information.&quot;
+                    </p>
+                    <p className="infoText">
+                        &quote;Limited knowledge of world events afgter 2021.&quot;
+                    </p>
+                </div>
+
+            </section>
+
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
