837d43784c27c9b7fcf113d5dbf83c78cd16d3c1
Hector Romero <hectorromerodev@gmail.com>
2023-02-12 22:01:08 -0800

    typings added and added sendMessage func

--- src/app/components/ChatInput.tsx
+++ src/app/components/ChatInput.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { addDoc, serverTimestamp, collection } from "@firebase/firestore";
 import { PaperAirplaneIcon } from "@heroicons/react/20/solid";
 import { useSession } from "next-auth/react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
+import toast from "react-hot-toast";
+import { db } from "../../../firebase";
 
 type Props = {
   chatId: string;
@@ -12,12 +15,63 @@ function ChatInput({ chatId }: Props) {
   const { data: session } = useSession();
   const [prompt, setPrompt] = useState("");
 
+  const model = "davinci";
+
+  const sendMessage = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!prompt) return;
+
+    const input = prompt.trim();
+    setPrompt("");
+
+    const message: Message = {
+      text: input,
+      createdAt: serverTimestamp(),
+      user: {
+        _id: session?.user?.email!,
+        name: session?.user?.name!,
+        avatar:
+          session?.user?.image ||
+          `https://ui-avatars.com/api/?name=${session?.user?.name}`,
+      },
+    };
+
+    await addDoc(
+      collection(
+        db,
+        "users",
+        session?.user?.email!,
+        "chats",
+        chatId,
+        "messages"
+      ),
+      message
+    );
+
+    const notification = toast.loading("ChatGTP is thinking...");
+
+    await fetch("/api/askQuestion", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: input,
+        chatId,
+        model,
+        session,
+      }),
+    }).then(() => {
+      toast.success("ChatGTP has responded!", { id: notification });
+    });
+  };
+
   return (
     <div
       className="bg-gray-700/50 text-gray-400 rounded-lg text-sm
       focus:outline-none"
     >
-      <form className="p-5 space-x-5 flex">
+      <form onSubmit={sendMessage} className="p-5 space-x-5 flex">
         <input
           className="bg-transparent flex-1 focus:outline-none
             disabled:cursor-not-allowed disabled:text-gray-300"
