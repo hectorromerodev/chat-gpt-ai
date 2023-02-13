96c177578849068d75a236c1e35fa309e1eb8853
Hector Romero <hectorromerodev@gmail.com>
2023-02-12 21:30:22 -0800

    update side bar to get messsages

--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -1,10 +1,22 @@
 "use client";
 
+import ChatRow from "@/app/components/ChatRow";
 import NewChat from "@/app/components/NewChat";
+import { collection, orderBy, query } from "@firebase/firestore";
 import { signOut, useSession } from "next-auth/react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../../../firebase";
 
 function SideBar() {
   const { data: session } = useSession();
+
+  const [chats] = useCollection(
+    session &&
+      query(
+        collection(db, "users", session?.user?.email!, "chats"),
+        orderBy("createdAt", "asc")
+      )
+  );
   return (
     <div className="p-2 flex flex-col h-screen">
       <div className="flex-1">
@@ -13,6 +25,9 @@ function SideBar() {
         <div>{/*Module Selection*/}</div>
 
         {/*Map through the ChatRows*/}
+        {chats?.docs.map((chat) => (
+          <ChatRow key={chat.id} id={chat.id} />
+        ))}
       </div>
       {session && (
         // eslint-disable-next-line @next/next/no-img-element
