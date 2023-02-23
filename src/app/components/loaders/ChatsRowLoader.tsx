1bd2cd0f715318906e7b56d0e4d4fbbd72a44e71
Hector Romero <hectorromerodev@gmail.com>
2023-02-22 23:42:31 -0800

    add skeletons loaders for chatRows and profile picture

---
+++ src/app/components/loaders/ChatsRowLoader.tsx
@@ -0,0 +1,23 @@
+const ChatsRowLoader = () => {
+  return (
+    <>
+      <div className="animate-pulse flex space-x-4 chatRow justify-center">
+        <div className="rounded-full bg-gray-400 h-6 w-6" />
+        <div className="h-5 bg-gray-400 rounded-sm w-3/4" />
+        <div className="rounded bg-gray-400 h-5 w-5" />
+      </div>
+      <div className="animate-pulse flex space-x-4 chatRow justify-center">
+        <div className="rounded-full bg-gray-400 h-6 w-6" />
+        <div className="h-5 bg-gray-400 rounded-sm w-3/4" />
+        <div className="rounded bg-gray-400 h-5 w-5" />
+      </div>
+      <div className="animate-pulse flex space-x-4 chatRow justify-center">
+        <div className="rounded-full bg-gray-400 h-6 w-6" />
+        <div className="h-5 bg-gray-400 rounded-sm w-3/4" />
+        <div className="rounded bg-gray-400 h-5 w-5" />
+      </div>
+    </>
+  );
+};
+
+export default ChatsRowLoader;
---
+++ src/app/components/loaders/ProfilePictureLoader.tsx
@@ -0,0 +1,8 @@
+const ProfilePictureLoader = () => {
+  return (
+    //   Loading a profile picture skeleton
+    <div className="h-12 w-12 rounded-full animate-pulse bg-gray-400 mx-auto mb-2" />
+  );
+};
+
+export default ProfilePictureLoader;
